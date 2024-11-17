e296960ecbaa1f644456f2ee8cbb8cd9eb0fd8c0
deepakbhimavarapu <deepakbhimavarapu@msitprogram.net>
2024-11-17 19:38:06 +0530

    added 4 posts to JSON

--- src/data/posts.js
+++ src/data/posts.js
@@ -19,5 +19,45 @@ export const posts = [
       tags: ["tag2"],
       category: 'technology'
     },
+    {
+      id: 3,
+      title: "Post3",
+      author: "Ajay",
+      content: "React is a powerful library for building user interfaces... React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[9][10] A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements. It was first launched on 29 May 2013.",
+      date: "2024-03-16",
+      readTime: 3,
+      tags: ["tag1"],
+      category: 'general'
+    },
+    {
+      id: 4,
+      title: "Post4",
+      author: "Vijay",
+      content: "React is a powerful library for building user interfaces... React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[9][10] A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements. It was first launched on 29 May 2013.",
+      date: "2024-03-16",
+      readTime: 3,
+      tags: ["tag2"],
+      category: 'technology'
+    },
+    {
+      id: 5,
+      title: "Post5",
+      author: "Raj",
+      content: "React is a powerful library for building user interfaces... React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[9][10] A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements. It was first launched on 29 May 2013.",
+      date: "2024-03-16",
+      readTime: 3,
+      tags: ["tag3"],
+      category: 'general'
+    },
+    {
+      id: 6,
+      title: "Post6",
+      author: "Ram",
+      content: "React is a powerful library for building user interfaces... React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[9][10] A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements. It was first launched on 29 May 2013.",
+      date: "2024-03-16",
+      readTime: 3,
+      tags: ["tag2", "tag1"],
+      category: 'technology'
+    },
     // Add 1-2 more sample posts
   ];
