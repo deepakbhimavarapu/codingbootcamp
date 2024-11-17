782e927772f91fe1ab55b5493658aaeb7a9ae0cb
deepakbhimavarapu <deepakbhimavarapu@msitprogram.net>
2024-11-17 20:06:00 +0530

    some changes

--- src/data/posts.js
+++ src/data/posts.js
@@ -2,7 +2,7 @@ export const posts = [
     {
       id: 1,
       title: "Getting Started with React",
-      content: "React is a powerful library for building user interfaces...",
+      content: "React is a powerful library for building user interfaces...React is a powerful library for building user interfacesReact is a powerful library for building user interfacesReact is a powerful library for building user interfaces",
       author: "Jane Doe",
       date: "2024-03-15",
       readTime: 5
@@ -10,7 +10,7 @@ export const posts = [
     {
       id: 2,
       title: "Understanding Props",
-      content: "Props are the way we pass data between React components...",
+      content: "Props are the way we pass data between React components...React is a powerful library for building user interfacesReact is a powerful library for building user interfacesReact is a powerful library for building user interfaces",
       author: "John Smith",
       date: "2024-03-16",
       readTime: 3
