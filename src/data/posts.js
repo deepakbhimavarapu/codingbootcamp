9daa42e8b720f1b0d08d623c725e931f5b596130
deepakbhimavarapu <deepakbhimavarapu@msitprogram.net>
2024-11-17 19:05:41 +0530

    Search, filters, pagination added

--- src/data/posts.js
+++ src/data/posts.js
@@ -5,14 +5,19 @@ export const posts = [
       content: "React is a powerful library for building user interfaces... React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[9][10] A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements. It was first launched on 29 May 2013.",
       author: "Jane Doe",
       date: "2024-03-15",
-      readTime: 5
+      readTime: 5,
+      tags: ["tag1"],
+      category: 'general'
     },
     {
       id: 2,
       title: "Understanding Props",
+      author: "Deepak",
       content: "React is a powerful library for building user interfaces... React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[9][10] A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements. It was first launched on 29 May 2013.",
       date: "2024-03-16",
-      readTime: 3
+      readTime: 3,
+      tags: ["tag2"],
+      category: 'technology'
     },
     // Add 1-2 more sample posts
   ];
